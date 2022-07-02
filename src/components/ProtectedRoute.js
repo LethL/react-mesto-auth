492d003f425362f6d09c2a3f72d6ede50b9a9cd2
Leth4L <carribean_08@mail.ru>
2022-07-02 15:24:30 +0300

    refactor: refactor readme.md

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -4,7 +4,7 @@ function ProtectedRoute ({ component: Component, ...props }) {
     return (
         <Route>
           {() =>
-            props.loggedIn ? <Component {...props} /> : <Redirect to="/signup" />
+            props.loggedIn ? <Component {...props} /> : <Redirect to="/signin" />
           }
         </Route>
     );
